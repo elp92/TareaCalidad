e9e51fe5040421bb0441a3199dcd350d6a1cc333
elp92 <elopezperez@hotmail.com>
2016-02-20 09:22:56 -0600

    Commit

---
+++ Calidad_Software1/CapaDatosUnityTest/AsociadoDALTests.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using CAPA_DATOS;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CAPA_DATOS.Tests
+{
+    [TestClass()]
+    public class AsociadoDALTests
+    {
+        [TestMethod()]
+        public void InsertarTest()
+        {
+            string path = "";
+            string filaName = "";
+            string extension = "";
+        }
+    }
+}
